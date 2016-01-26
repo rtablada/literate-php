b6ce6846defd80c9b74193feefd35c479b9f0799
Benedict Roeser <mail@benedictroeser.de>
2016-01-26 10:43:47 +0100

    Added PHPDoc comments

--- src/Parser.php
+++ src/Parser.php
@@ -1,7 +1,17 @@
 <?php namespace Rtablada\LiteratePhp;
 
+/**
+ * This Parser can be used to parse Markdown syntax that contains blocks of PHP
+ * code
+ */
 class Parser
 {
+    /**
+     * Parses a string in Markdown syntax and returns PHP code
+     * 
+     * @param string $string
+     * @return string
+     */
 	public function parse($string)
 	{
 		$string = $this->breakParts($string);
@@ -9,11 +19,30 @@ class Parser
 		return "<?php\n\n".$string;
 	}
 
+    /**
+     * Removes lists that are created via Markdown * (star) syntax from a string
+     * of Markdown.
+     * 
+     * Useful because in PHP comments, each line of a multi-line
+     * comment begins with a star. To keep your Markdown lists intact within the
+     * comments, you can use the - (minus) symbol instead of the star within
+     * your Markdown code.
+     * 
+     * @param type $string
+     * @return type
+     */
 	public function removeLists($string)
 	{
 		return preg_replace('/[\n]*\t*\*.*/', '', $string);
 	}
 
+    /**
+     * Breaks a string in Markdown syntax into parts. Every other part is PHP
+     * code, the rest is Markdown and will be turned into PHP comments
+     * 
+     * @param string $string
+     * @return string
+     */
 	public function breakParts($string)
 	{
 		$parts = preg_split('/[\n]+(```|~~~).*[\n]/', $string);
@@ -37,6 +66,13 @@ class Parser
 	}
 
 
+    /**
+     * Parses a string of Markdown and returns a PHP comment block containing
+     * its text
+     * 
+     * @param string $string
+     * @return string
+     */
 	public function parseComments($string)
 	{
 		$return = '';
@@ -62,6 +98,13 @@ class Parser
 		return $return;
 	}
 
+    /**
+     * Parses a block of PHP code (inside a Markdown string), e. g. removes
+     * the leading <?php (if any)
+     * 
+     * @param string $string
+     * @return string
+     */
 	public function parseCode($string)
 	{
 		$string = preg_replace('/<\?php\s/', '', $string);
